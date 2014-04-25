8fdb851fc113a85c2fe13d5383379c103217dd07
Shaun August <shaun@eoslightmedia.com>
2014-04-25 13:03:52 -0700

    fixes

--- Rakefile
+++ Rakefile
@@ -2,9 +2,13 @@ $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/osx'
 require 'rubygems'
 
+unless defined?(Motion::Project::Config)
+  raise "This file must be required within a RubyMotion project Rakefile."
+end
 
 Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'rubymotion-graphics/*.rb')).each do |file|
     app.files.unshift(file)
   end
+  app.frameworks += ['Quartz']
 end
--- lib/rubymotion-graphics.rb
+++ lib/rubymotion-graphics.rb
@@ -7,7 +7,7 @@ require './lib/rubymotion-graphics'
 
 
 Motion::Project::App.setup do |app|
-  
+
     app.frameworks << 'Cocoa' unless app.frameworks.include?('Cocoa')
     app.frameworks << 'Quartz' unless app.frameworks.include?('Quartz')
     app.frameworks << 'CoreGraphics' unless app.frameworks.include?('CoreGraphics')
@@ -15,8 +15,7 @@ Motion::Project::App.setup do |app|
     # p Dir.glob(File.join(File.dirname(__FILE__))
     Dir.glob(File.join(File.dirname(__FILE__), 'lib/rubymotion-graphics/*.rb')).each do |file|
       app.files.unshift(file)
-    end 
+    end
 end
 
-require 'rubymotion-graphics/canvas.rb'
-
+# require 'rubymotion-graphics/canvas.rb'
